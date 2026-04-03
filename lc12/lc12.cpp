73e18199d964d8efa7c7d2f8edf9d1119a697950
Rakshan <rakshan.cod56@gmail.com>
2026-04-03 23:23:00 +0530

    adding question 12 to the repository

---
+++ lc12/lc12.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string intToRoman(int num) {
+    static vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+    static vector<string> sym{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    string res;
+    for(int i = 0; num > 0; i++){
+        if(num == 0){
+            break;
+        }
+        int times = num / val[i];
+
+        while(times--){
+            res += sym[i];
+        }
+        num = num % val[i];
+    }
+    return res;
+}
+
+int main(){
+    int num = 45;
+
+    string result = intToRoman(num);
+
+    cout<<"the roman numeral of "<<num<<" is "<<result<<endl;
+
+    return 0;
+}
